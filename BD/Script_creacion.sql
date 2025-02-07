c70d1f10207a76f2504f907379b303a48c709b69
IsmaBH <deigo-b@hotmail.com>
2025-02-07 13:58:51 -0600

    Se corrige el script que genera la base de datos con el modelo actualizado

--- BD/Script_creacion.sql
+++ BD/Script_creacion.sql
@@ -18,38 +18,14 @@ CREATE SCHEMA IF NOT EXISTS `jardin_floral_bd` DEFAULT CHARACTER SET utf8mb4 COL
 USE `jardin_floral_bd` ;
 
 -- -----------------------------------------------------
--- Table `jardin_floral_bd`.`usuario`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `jardin_floral_bd`.`usuario` (
-  `usuarioID` INT NOT NULL AUTO_INCREMENT,
-  `nombre` VARCHAR(45) NOT NULL,
-  `apellido_p` VARCHAR(45) NOT NULL,
-  `apellido_m` VARCHAR(45) NULL DEFAULT NULL,
-  `telefono` BIGINT NOT NULL,
-  `correo` VARCHAR(55) NOT NULL,
-  PRIMARY KEY (`usuarioID`),
-  UNIQUE INDEX `UsuarioID_UNIQUE` (`usuarioID` ASC) VISIBLE)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
-
--- -----------------------------------------------------
 -- Table `jardin_floral_bd`.`pedidos`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `jardin_floral_bd`.`pedidos` (
   `transaccionID` INT NOT NULL AUTO_INCREMENT,
-  `fecha_venta` DATE NOT NULL,
-  `cantidad_pagada` INT NOT NULL,
-  `cantidad_vendida` INT NOT NULL,
-  `usuario_usuarioID` INT NOT NULL,
-  PRIMARY KEY (`transaccionID`, `usuario_usuarioID`),
-  INDEX `fk_pedidos_usuario1_idx` (`usuario_usuarioID` ASC) VISIBLE,
-  CONSTRAINT `fk_pedidos_usuario1`
-    FOREIGN KEY (`usuario_usuarioID`)
-    REFERENCES `jardin_floral_bd`.`usuario` (`usuarioID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+  `fechaventa` DATE NOT NULL,
+  `cantidadpagada` DOUBLE NOT NULL,
+  `cantidadvendida` INT NOT NULL,
+  PRIMARY KEY (`transaccionID`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
@@ -59,13 +35,14 @@ COLLATE = utf8mb4_0900_ai_ci;
 -- Table `jardin_floral_bd`.`productos`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `jardin_floral_bd`.`productos` (
-  `productosID` INT NOT NULL AUTO_INCREMENT,
-  `nombre_producto` VARCHAR(45) NOT NULL,
-  `precio` VARCHAR(45) NOT NULL,
+  `productosID` BIGINT(12) NOT NULL,
+  `nombreproducto` VARCHAR(45) NOT NULL,
+  `precio` DOUBLE NOT NULL,
   `color` VARCHAR(45) NOT NULL,
-  `stock` VARCHAR(45) NOT NULL,
-  `descripcion` VARCHAR(100) NOT NULL,
+  `stock` INT NOT NULL,
+  `descripcion` VARCHAR(300) NOT NULL,
   `temporada` VARCHAR(45) NOT NULL,
+  `urlimagen` VARCHAR(150) NOT NULL,
   PRIMARY KEY (`productosID`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
@@ -73,27 +50,34 @@ COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
--- Table `jardin_floral_bd`.`productos_has_pedidos`
+-- Table `jardin_floral_bd`.`usuario`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `jardin_floral_bd`.`productos_has_pedidos` (
-  `productos_productosID` INT NOT NULL,
-  `pedidos_transaccionID` INT NOT NULL,
-  PRIMARY KEY (`productos_productosID`, `pedidos_transaccionID`),
-  INDEX `fk_productos_has_pedidos_pedidos1_idx` (`pedidos_transaccionID` ASC) VISIBLE,
-  INDEX `fk_productos_has_pedidos_productos1_idx` (`productos_productosID` ASC) VISIBLE,
-  CONSTRAINT `fk_productos_has_pedidos_productos1`
-    FOREIGN KEY (`productos_productosID`)
-    REFERENCES `jardin_floral_bd`.`productos` (`productosID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_productos_has_pedidos_pedidos1`
-    FOREIGN KEY (`pedidos_transaccionID`)
-    REFERENCES `jardin_floral_bd`.`pedidos` (`transaccionID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+CREATE TABLE IF NOT EXISTS `jardin_floral_bd`.`usuario` (
+  `usuarioID` INT NOT NULL,
+  `nombre` VARCHAR(45) NOT NULL,
+  `apellidop` VARCHAR(45) NOT NULL,
+  `apellidom` VARCHAR(45) NULL DEFAULT NULL,
+  `telefono` BIGINT NOT NULL,
+  `correo` VARCHAR(55) NOT NULL,
+  `contraseña` VARCHAR(150) NOT NULL,
+  `rol` VARCHAR(10) NOT NULL,
+  PRIMARY KEY (`usuarioID`),
+  UNIQUE INDEX `UsuarioID_UNIQUE` (`usuarioID` ASC) VISIBLE)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `jardin_floral_bd`.`categorias`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `jardin_floral_bd`.`categorias` (
+  `idcategorias` INT NOT NULL,
+  `nombre` VARCHAR(45) NOT NULL,
+  `descripcion` VARCHAR(300) NOT NULL,
+  PRIMARY KEY (`idcategorias`),
+  UNIQUE INDEX `idcategorias_UNIQUE` (`idcategorias` ASC) VISIBLE)
+ENGINE = InnoDB;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
