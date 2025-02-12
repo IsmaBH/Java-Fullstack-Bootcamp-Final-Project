c2c24f4d77792b01f82eafd2357930c64e542b29
Jackeline Montalvo <jackeline.montalvog@gmail.com>
2025-02-12 14:10:56 -0600

    Se agregan archivos de Pedidos

--- ecommerce/src/main/java/com/jardinfloral/ecommerce/controller/PedidosController.java
+++ ecommerce/src/main/java/com/jardinfloral/ecommerce/controller/PedidosController.java
@@ -10,44 +10,47 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jardinfloral.ecommerce.model.Pedido;
 import com.jardinfloral.ecommerce.service.PedidosService;
 
 @RestController
-@RequestMapping(path="/api/pedidos/")
+@RequestMapping(path = "/api/pedidos/")
 public class PedidosController {
 	private final PedidosService pedidosService;
-	
+
 	@Autowired
 	public PedidosController(PedidosService pedidoService) {
 		this.pedidosService = pedidoService;
 	}
-	
+
 	@GetMapping
-	public List<Pedido> getPedidos(){
+	public List<Pedido> getPedidos() {
 		return pedidosService.getAllPedidos();
 	}
-	
-	@GetMapping(path="{pedidoId}")
+
+	@GetMapping(path = "{pedidoId}")
 	public Pedido getPedido(@PathVariable("pedidoId") Integer id) {
 		return pedidosService.getPedido(id);
 	}
-	
-	@DeleteMapping(path="{pedidoId}")
+
+	@DeleteMapping(path = "{pedidoId}")
 	public Pedido deletePedido(@PathVariable("pedidoId") Integer id) {
 		return pedidosService.deletePedido(id);
 	}
-	
+
 	@PostMapping
 	public Pedido addPedido(@RequestBody Pedido pedido) {
 		return pedidosService.addPedido(pedido);
 	}
-	/*
-	@PutMapping(path="{pedidoId}")
-	public Pedido updatePedido(@PathVariable("pedidoId") Integer id) {
-		return pedidosService.updatePedido(id);
-	}
-	*/
+
+	@PutMapping(path = "{pedidoId}")
+	public Pedido updatePedido(@PathVariable("pedidoId") Integer id, 
+			@RequestParam(required = false) Double cantidadPagada,
+			@RequestParam(required = false) Integer cantidadVendida) {
+		return pedidosService.updatePedido(id, cantidadPagada, cantidadVendida);
+	}// update
+
 }
--- ecommerce/src/main/java/com/jardinfloral/ecommerce/service/PedidosService.java
+++ ecommerce/src/main/java/com/jardinfloral/ecommerce/service/PedidosService.java
@@ -19,12 +19,12 @@ public class PedidosService {
 	
 	public List<Pedido> getAllPedidos(){
 		return pedidosRepository.findAll();
-	}
+	}// getAllPedidos
 	
 	public Pedido getPedido(Integer id) {
 		return pedidosRepository.findById(id).orElseThrow(
 				() -> new IllegalArgumentException("El pedido con el id: ["+id+"] no existe."));
-	}
+	}// getPedido
 	
 	public Pedido deletePedido(Integer id) {
 		Pedido ped = null;
@@ -33,9 +33,22 @@ public class PedidosService {
 			pedidosRepository.deleteById(id);
 		}
 		return ped;
-	}
+	}// deletePedido
 	
 	public Pedido addPedido(Pedido pedido) {
 		return pedidosRepository.save(pedido);
-	}
-}
+	}// addPedido
+
+	public Pedido updatePedido(Integer id, Double cantidadPagada, Integer cantidadVendida) {
+		Pedido ped = null;
+		if(pedidosRepository.existsById(id)) {
+			Pedido pedido = pedidosRepository.findById(id).get();
+			if(cantidadPagada != null) pedido.setCantidadPagada(cantidadPagada);
+			if(cantidadVendida != null) pedido.setCantidadVendida(cantidadVendida);
+			pedidosRepository.save(pedido);
+			ped = pedido;
+		}
+    	return ped;
+	}// updatePedido
+	
+}// PedidosService
