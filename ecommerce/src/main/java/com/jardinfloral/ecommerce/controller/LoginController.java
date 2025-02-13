9796cd4584817903e805fed391991556c5cac3a4
Katherine RA <kathephone1805@gmail.com>
2025-02-13 15:41:26 -0600

    Se carga el front en la carpeta static

--- ecommerce/src/main/java/com/jardinfloral/ecommerce/controller/LoginController.java
+++ ecommerce/src/main/java/com/jardinfloral/ecommerce/controller/LoginController.java
@@ -6,7 +6,6 @@ import java.util.Date;
 import javax.servlet.ServletException;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
--- ecommerce/src/main/java/com/jardinfloral/ecommerce/controller/ProductosController.java
+++ ecommerce/src/main/java/com/jardinfloral/ecommerce/controller/ProductosController.java
@@ -2,7 +2,6 @@ package com.jardinfloral.ecommerce.controller;
 
 //Mismamente aqui tambien solo traer las librerias necesarias evitar el uso del *
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
